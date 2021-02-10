be71e5378c15122ad2f28671219d2ef6e9c2b3cb
apsius <38158216+appsius@users.noreply.github.com>
2021-02-10 09:13:03 +0300

    displayed map using Leaflet.js

--- script.js
+++ script.js
@@ -14,37 +14,80 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // ***********************************************************************//
 // Get Current Location
 // ******************************* 1 ************************************//
-//navigator.geolocation.getCurrentPosition(
-//  function (position) {
-//    const { latitude } = position.coords;
-//    const { longitude } = position.coords;
-//    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-//  },
-//  function () {
-//    alert('Your current position is not available!');
-//  }
-//);
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Your current position is not available!');
+    }
+  );
 
 // ******************************* 2 ************************************//
-//navigator.geolocation.getCurrentPosition(
-//  function (location) {
-//    const { latitude } = location.coords;
-//    const { longitude } = location.coords;
-//    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-//  },
-//  function () {
-//    alert('Position is not available!!!');
-//  }
-//);
+//if (navigator.geolocation)
+//  navigator.geolocation.getCurrentPosition(
+//    function (location) {
+//      const { latitude } = location.coords;
+//      const { longitude } = location.coords;
+
+//      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+//      const coords = [latitude, longitude];
+//      const map = L.map('map').setView(coords, 13);
+
+//      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//        attribution:
+//          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//      }).addTo(map);
+//      L.marker(coords)
+//        .addTo(map)
+//        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//        .openPopup();
+//    },
+//    function () {
+//      alert('Position is not available!!!');
+//    }
+//  );
 
 // ******************************* 3 ************************************//
-navigator.geolocation.getCurrentPosition(
-  function (loc) {
-    const { latitude } = loc.coords;
-    const { longitude } = loc.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-  },
-  function () {
-    alert('Position not available!!!');
-  }
-);
+//if (navigator.geolocation)
+//  navigator.geolocation.getCurrentPosition(
+//    function (loc) {
+//      const { latitude } = loc.coords;
+//      const { longitude } = loc.coords;
+//      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+//      const coords = [latitude, longitude];
+
+//      const map = L.map('map').setView(coords, 13);
+
+//      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//        attribution:
+//          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//      }).addTo(map);
+
+//      L.marker(coords)
+//        .addTo(map)
+//        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//        .openPopup();
+//    },
+//    function () {
+//      alert('Position not available!!!');
+//    }
+//  );
