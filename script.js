5c72b7446420b09c4e82919c39a185a48e3ff0c3
apsius <38158216+appsius@users.noreply.github.com>
2021-02-10 08:44:11 +0300

    get geolocation

--- script.js
+++ script.js
@@ -10,3 +10,41 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// ***********************************************************************//
+// Get Current Location
+// ******************************* 1 ************************************//
+//navigator.geolocation.getCurrentPosition(
+//  function (position) {
+//    const { latitude } = position.coords;
+//    const { longitude } = position.coords;
+//    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+//  },
+//  function () {
+//    alert('Your current position is not available!');
+//  }
+//);
+
+// ******************************* 2 ************************************//
+//navigator.geolocation.getCurrentPosition(
+//  function (location) {
+//    const { latitude } = location.coords;
+//    const { longitude } = location.coords;
+//    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+//  },
+//  function () {
+//    alert('Position is not available!!!');
+//  }
+//);
+
+// ******************************* 3 ************************************//
+navigator.geolocation.getCurrentPosition(
+  function (loc) {
+    const { latitude } = loc.coords;
+    const { longitude } = loc.coords;
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+  },
+  function () {
+    alert('Position not available!!!');
+  }
+);
